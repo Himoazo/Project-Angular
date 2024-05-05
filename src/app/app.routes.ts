982a33adbac661d801c59ccb54bb6592cd9c65b8
Hoomam <hoaz1600@student.miun.se>
2024-05-06 00:37:49 +0200

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { DiagramComponent } from './diagram/diagram.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
-    {path: "courses", component: CoursesComponent}
+    {path: "home", component: HomeComponent},
+    {path: "courses", component: CoursesComponent},
+    {path: "diagram", component: DiagramComponent}
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "404", component: NotfoundComponent},
+    {path: "**", component: NotfoundComponent}
 ];
