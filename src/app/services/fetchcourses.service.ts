9615eb426da01e9fda21ebdc8374f805771541f9
Hoomam <hoaz1600@student.miun.se>
2024-05-05 11:10:01 +0200

    Service

--- src/app/services/fetchcourses.service.ts
+++ src/app/services/fetchcourses.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from '../model/course';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +9,9 @@ import { Injectable } from '@angular/core';
 export class FetchcoursesService {
 
   private url: string = "https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
 }
