af08333d4ac0e8cf532dff241e4bf97b3f42a131
Hoomam <hoaz1600@student.miun.se>
2024-05-05 18:25:52 +0200

    ngOnInit courses component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { Course } from '../model/course';
+import { FetchcoursesService } from '../services/fetchcourses.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+  courses: Course[] = [];
 
+  constructor(private courseservice: FetchcoursesService){}
+
+  ngOnInit(){
+    this.courseservice.getCourses().subscribe(data =>{
+      this.courses = data;
+    });
+  }
 }
