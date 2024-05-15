739966caa552a711e5d1d7573928d8e185de29a7
Hoomam <hoaz1600@student.miun.se>
2024-05-15 22:09:29 +0200

    More styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  uniStudents = "assets/uniStudents.jpg";
+  uniPic = "assets/uni2.jpg";
+  gradStudents = "assets/graduation.jpg";
+  books = "assets/courseBooks.jpg";
+  video = "assets/videoAngular.mp4";
 }
